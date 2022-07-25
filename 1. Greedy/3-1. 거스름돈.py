3f5ee5fad4ae97d432ae17cce523115c3fa386dd
박현민 <dndb4902@naver.com>
2022-07-25 18:14:43 +0900

    2022-07-25

--- 1. Greedy/3-1. 거스름돈.py
+++ 1. Greedy/3-1. 거스름돈.py
@@ -1,3 +1,6 @@
+# 예제 3-1. 거스름돈
+# 가장 큰 화폐 단위부터 돈을 거슬러주기
+
 n = 1260
 count = 0
 
@@ -5,10 +8,11 @@ count = 0
 coin_types = [500, 100, 50, 10]
 
 for coin in coin_types:
-  count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
-  n %= coin
+    count += n // coin  # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    n %= coin
 
 print(count)
+print(11111)
 
 # 시간 복잡도 O(K)
 # : 동전의 총 종류에만 영향을 받는다.
--- 1. Greedy/3-2. 큰 수의 법칙_단순.py
+++ 1. Greedy/3-2. 큰 수의 법칙_단순.py
@@ -1,23 +1,27 @@
+# 예제 3-2. 큰 수의 법칙
+# N 크기의 배열을 입력받아서 주어진 수를 M번 더하여 가장 큰 수가 나와야하지만
+# K 번을 초과해서 더해질 순 없다
+
 # N, M, K를 공백으로 구분하여 입력받기
 n, m, k = map(int, input().split())
 # N개의 수를 공백으로 구분하여 입력받기
 data = list(map(int, input().split))
 
-data.sort() # 입력받은 수들 정렬하기
-first = data[n-1] # 가장 큰 수
-second = data[n-2] # 두 번째로 큰 수
+data.sort()  # 입력받은 수들 정렬하기
+first = data[n - 1]  # 가장 큰 수
+second = data[n - 2]  # 두 번째로 큰 수
 
 result = 0
 
 while True:
-  for i in range(k): # 가장 큰 수를 K번 더하기
-    if m==0:  # m이 0이라면 반복문 탈출
-      break
-    result += first
-    m -= -1  # 더할 때마다 1씩 빼기
-  if m==0: # m이 0이라면 반복문 탈출
-    break
-  result += second # 두 번째로 큰 수를 한 번 더하기
-  m -= 1  # 더할 때마다 1씩 빼기
+    for i in range(k):  # 가장 큰 수를 K번 더하기
+        if m == 0:  # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= -1  # 더할 때마다 1씩 빼기
+    if m == 0:  # m이 0이라면 반복문 탈출
+        break
+    result += second  # 두 번째로 큰 수를 한 번 더하기
+    m -= 1  # 더할 때마다 1씩 빼기
 
 print(result)  # 최종답안 출력

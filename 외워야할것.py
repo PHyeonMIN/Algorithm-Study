a7e65d7d4ca091de0b8c4003e2025dd83e623284
박현민 <dndb4902@naver.com>
2023-05-02 22:08:27 +0900

    #코테를 위해 외워야할 것들

---
+++ 외워야할것.py
@@ -0,0 +1,49 @@
+# 1. 빠르게 입력받기
+import sys
+# 하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
+
+# 입력받은 문자열 그대로 출력
+print(input_data)
+
+
+
+# 2. 계수 정렬 소스코드 ( 퀵정렬 보다 빠른 정렬이 필요할 때 사용 )
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+count = [0] * (max(array) + 1)
+for i in range(len(array)):
+    count[array[i]] += 1    # 각 데이터에 해당하는 인덱스의 값 증가
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=' ')
+
+
+# 3. 이진 탐색
+# 이진 탐색 소스코드 구현(재귀함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid -1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target, mid + 1, end)
+# 이진 탐색 소스코드 구현(반복문)
+def binary_search2(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+        else:
+            start = mid + 1
+    return None
